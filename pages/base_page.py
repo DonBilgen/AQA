9ba8e973e864d4c0f09ac9a8bb0e91ac64383521
DonBilgen <don.bilgen@gmail.com>
2023-05-12 22:08:58 +0300

    Add test TestFORM

--- pages/base_page.py
+++ pages/base_page.py
@@ -42,4 +42,8 @@ class BasePage:
         action.context_click(element)
         action.perform()
 
+    def remove_footer(self):
+        self.driver.execute_script("document.getElementsByTagName('footer')[0].remove();")
+        # self.driver.execute_script("document.getElementsById('close-fixedban').remove();")
+
     # def switch_to_new_tab(self):
